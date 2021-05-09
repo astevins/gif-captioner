5de6a0a0ac556a1b2d50a41c58b6e0ebae57714d
astevins <arya.stevinson@alumni.ubc.ca>
2021-05-08 23:01:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gif Captioner
+
+This project is currently in progress.
+
+### Purpose
+The site will allow a user to upload a gif, add a caption to it, and download the captioned gif.
+### Build
+Gif Captioner uses a React frontend and Node.js backend with Feathers for API.
---
+++ client/src/components/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+import GifUploader from "./GifUploader";
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Gif Captioner</h1>
+            <GifUploader/>
+            <div className="attribution-footer">
+                <p>
+                    Icons made by
+                    <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+                    from
+                    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com </a>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
