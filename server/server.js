4372560643a9b5b172b04306c5b7abea55d99b9f
astevins <arya.stevinson@alumni.ubc.ca>
2021-05-12 18:06:20 -0400

    Remove unnecessary module imports from server.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,16 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const config = require('dotenv').config()
 
 const app = express();
 
-// view engine setup
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
